335ff9662ed280db73689ec18675b2aed8ff9bed
Shubhamvumap123 <94427228+Shubhamvumap123@users.noreply.github.com>
2022-06-06 14:43:55 +0530

    comit

---
+++ Backened/src/models/product.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const proSchema = new mongoose.Schema({
+    
+  "id": { type: Number, required: false },
+  "img":  { type: String, required: true },
+  "type": { type: String, required: true },
+  "color":{ type: String, required: true },
+  "year": { type: Number, required: true }
+});
+ 
+const Product = mongoose.model("product", proSchema);
+
+module.exports = Product;
