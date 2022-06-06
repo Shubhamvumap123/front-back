16f43547a4580b15f1a3ed05a6bcc6e97fecc3bd
Shubhamvumap123 <94427228+Shubhamvumap123@users.noreply.github.com>
2022-06-06 15:28:44 +0530

    comit

--- Backened/src/controller/product.controller.js
+++ Backened/src/controller/product.controller.js
@@ -3,5 +3,9 @@ const { router } = require('..');
 const product = require('../models/product.model');
 
 router.get("/carpro", async (req, res) => {
-   
+   try {
+     res.status(200).send();
+   } catch (error) {
+     console.log("error", error);
+   } 
 })
